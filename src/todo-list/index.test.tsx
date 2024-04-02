e3a7362be0711a87f72576557ecb628f27b271bf
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-04-02 16:17:23 +0900

    Feat: 테스트 케이스 추가 및 액션 함수 재사용

--- src/todo-list/index.test.tsx
+++ src/todo-list/index.test.tsx
@@ -1,21 +1,60 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import ToDoList from ".";
 
-describe("", () => {
-  test("", () => {
-    render(<ToDoList />);
-    const input = screen.getByPlaceholderText("할 일을 작성해 주세요.") as HTMLInputElement;
-    fireEvent.change(input, { target: { value: "첫 번째" } });
-    expect(input).toHaveAttribute("value", "첫 번째");
+const renderToDoList = () => {
+  const { container } = render(<ToDoList />);
+  const input = screen.getByPlaceholderText("할 일을 작성해 주세요.") as HTMLInputElement;
+  return { container, input };
+};
 
-    fireEvent.keyDown(input, { key: "Enter" });
+const writeToDo = (input: HTMLInputElement, value: string) => {
+  fireEvent.change(input, { target: { value: value } });
+  expect(input).toHaveAttribute("value", value);
+};
 
-    const todoList = screen.getByText("첫 번째");
-    expect(todoList).toBeVisible();
-    expect(input).toHaveAttribute("value", "");
+const addToDO = (input: HTMLInputElement, value: string) => {
+  fireEvent.keyDown(input, { key: "Enter" });
+  const todoList = screen.getByText(value);
+  expect(todoList).toBeVisible();
+  expect(input).toHaveAttribute("value", "");
+};
 
-    const checkbox = screen.getByLabelText("0") as HTMLInputElement;
-    fireEvent.change(checkbox, { target: { name: "0", checked: !checkbox.checked } });
-    expect(checkbox.checked).toBe(true);
+const checkToDo = (checkbox: HTMLInputElement, name: string, result: boolean) => {
+  fireEvent.change(checkbox, { target: { name: name, checked: !checkbox.checked } });
+  expect(checkbox.checked).toBe(result);
+};
+
+describe("ToDo 리스트 화면 동작 테스트", () => {
+  test("ToDo 리스트 입력 동작 테스트", () => {
+    const { input } = renderToDoList();
+    writeToDo(input, "첫 번째");
+  });
+
+  test("ToDo 리스트 추가 동작 테스트", () => {
+    const { input } = renderToDoList();
+    writeToDo(input, "첫 번째");
+    addToDO(input, "첫 번째");
+    writeToDo(input, "두 번째");
+    addToDO(input, "두 번째");
+  });
+
+  test("ToDo 리스트 체크박스 동작 테스트", () => {
+    const { input } = renderToDoList();
+    writeToDo(input, "첫 번째");
+    addToDO(input, "첫 번째");
+
+    writeToDo(input, "두 번째");
+    addToDO(input, "두 번째");
+
+    checkToDo(screen.getByLabelText("0") as HTMLInputElement, "0", true);
+  });
+
+  test("ToDo 리스트 체크박스 더블 클릭 테스트", () => {
+    const { input } = renderToDoList();
+    writeToDo(input, "첫 번째");
+    addToDO(input, "첫 번째");
+
+    checkToDo(screen.getByLabelText("0") as HTMLInputElement, "0", true);
+    checkToDo(screen.getByLabelText("0") as HTMLInputElement, "0", false);
   });
 });
